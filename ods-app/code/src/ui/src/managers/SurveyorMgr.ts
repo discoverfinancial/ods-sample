bd9b1366a4d392f8cfaf248548f2637e52ee7814
Bryce Curtis <brycecurtis@discover.com>
2026-06-11 10:23:26 -0400

    Update SurveyorMgr.ts
    Signed-off-by: Bryce Curtis <brycecurtis@discover.com>

--- ods-app/code/src/ui/src/managers/SurveyorMgr.ts
+++ ods-app/code/src/ui/src/managers/SurveyorMgr.ts
@@ -84,7 +84,83 @@ export class SurveyorMgr {
         }
     }
 
+    /**
+     * Process a command with post
+     * 
+     * @param leanixId id of the LeanIx doc
+     * @param nexusId if os the Nexus doc
+     * @returns 
+     */
+    async linkSboms(leanixId: string, nexusId: string): Promise<any> {
+        try {
+            const response = await this.http.post(`${REACT_APP_ODS_SERVER}/api/surveyor/linkSboms/${leanixId}/${nexusId}`);
+            console.log("linkSboms response=", response)
+            const body = response.data;
+            return body;
+        } catch (e: any) {
+            const err = new DocError(e);
+            console.error(err);
+            throw err;
+        }
+    }
+
+    /**
+     * Process a command with post
+     * 
+     * @param sbomId id of the sbom
+     * @returns 
+     */
+    async unlinkSbom(sbomId: string): Promise<any> {
+        try {
+            const response = await this.http.post(`${REACT_APP_ODS_SERVER}/api/surveyor/unlinkSbom/${sbomId}`);
+            console.log("unlinkSbom response=", response)
+            const body = response.data;
+            return body;
+        } catch (e: any) {
+            const err = new DocError(e);
+            console.error(err);
+            throw err;
+        }
+    }
+
+    async getSandboxFunctions(): Promise<any> {
+        try {
+            const response = await this.http.get(`${REACT_APP_ODS_SERVER}/api/surveyor/sandboxFunctions`);
+            console.log("getSandboxFunctions response=", response)
+            const body = response.data;
+            return body;
+        } catch (e: any) {
+            const err = new DocError(e);
+            console.error(err);
+            throw err;
+        }
+    }
+
+    async setSandboxFunctions(js: string): Promise<any> {
+        try {
+            const response = await this.http.post(`${REACT_APP_ODS_SERVER}/api/surveyor/sandboxFunctions`, {js:js});
+            console.log("setSandboxFunctions response=", response)
+            const body = response.data;
+            return body;
+        } catch (e: any) {
+            const err = new DocError(e);
+            console.error(err);
+            throw err;
+        }
+    }
     
+    async resetSandboxFunctions(): Promise<any> {
+        try {
+            const response = await this.http.post(`${REACT_APP_ODS_SERVER}/api/surveyor/sandboxFunctionsReset`);
+            console.log("resetSandboxFunctions response=", response)
+            const body = response.data;
+            return body;
+        } catch (e: any) {
+            const err = new DocError(e);
+            console.error(err);
+            throw err;
+        }
+    }    
 }
 
 
